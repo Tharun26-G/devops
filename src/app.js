0858e8e45170c550c69994906959b07258cf7af3
Tharun26-G <tharun26.g@gmail.com>
2025-10-04 19:20:34 +0530

    logger & middleware setup

--- src/app.js
+++ src/app.js
@@ -1,10 +1,24 @@
 import express from 'express';
+import logger from '#config/logger.js';
+import helmet from "helmet";
+import morgan from "morgan";
+import cors from "cors";
+import cookieParser from "cookie-parser";
 
 const app = express();
+
+
+app.use(helmet());
+app.use(cors());
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan('combined', { stream: {write: (message) => logger.info(message.trim())}}));
 
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
+  logger.info('Hello from logger');
   res.send('Hello World');
 });
 
